e50bd222bffdc669dc76a567d853be2d9baa5e70
Felipe Faggian <felipesantosfaggian@gmail.com>
2023-05-10 14:02:36 -0300

    Exercise Update Database

--- application/Program.java
+++ application/Program.java
@@ -2,10 +2,8 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import db.DB;
 
@@ -13,40 +11,25 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
 
 		Connection conn = null;
-		ResultSet rs = null;
 		PreparedStatement st = null;
-
 		try {
 			conn = DB.getConnection();
 
-			/*
-			 * pst = conn.prepareStatement("INSERT INTO seller" +
-			 * "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES " +
-			 * "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			 * 
-			 * pst.setString(1, "Carl Purlpe"); pst.setString(2, "carl@gmail.com");
-			 * pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			 * pst.setDouble(4, 3000.0); pst.setInt(5, 4);
-			 */
+			st = conn.prepareStatement(
+					"UPDATE seller " 
+					+ "SET BaseSalary = BaseSalary + ? " 
+					+ "WHERE " 
+					+ "(DepartmentId = ?)");
 
-			st = conn.prepareStatement("insert into department (Name) values ('D1'), ('D2')",
-					Statement.RETURN_GENERATED_KEYS);
+			st.setDouble(1, 200.00);
+			st.setInt(2, 2);
 
 			int rowsAffected = st.executeUpdate();
 
-			if (rowsAffected > 0) {
-				rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			} else {
-				System.out.println("No rows affected! ");
-			}
-
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
